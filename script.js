a4a3289f64032b147ad6f98622926d4b35b2d218
Yeabtsega Tesfaye <yeabtsegayeab85.@gmail.com>
2024-11-03 04:21:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const sounds = ['JucieWRLD', 'Billie', 'Ed-Sheeran',]
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerHTML = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').
+        appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
